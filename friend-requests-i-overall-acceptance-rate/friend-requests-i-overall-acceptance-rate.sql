bb69cf43128019569ae4062b822aa96dc59a938d
Erica Ouyang <93496446+EO1026@users.noreply.github.com>
2021-12-19 12:23:29 -0500

    Time: 394 ms (86.93%), Space: 0B (100.00%) - LeetHub

---
+++ friend-requests-i-overall-acceptance-rate/friend-requests-i-overall-acceptance-rate.sql
@@ -0,0 +1,3 @@
+# Write your MySQL query statement below
+SELECT ROUND(IFNULL((SELECT COUNT(*) FROM (SELECT DISTINCT requester_id, accepter_id FROM RequestAccepted)as a)
+    /(SELECT COUNT(*) FROM (SELECT DISTINCT sender_id, send_to_id FROM FriendRequest) as b),0), 2) AS accept_rate;
