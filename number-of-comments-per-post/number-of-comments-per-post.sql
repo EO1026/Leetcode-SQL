dc42f0b07e11a965992e14bfc221afbf4e08b522
Erica Ouyang <93496446+EO1026@users.noreply.github.com>
2021-12-23 11:25:44 -0500

    Time: 1069 ms (87.64%), Space: 0B (100.00%) - LeetHub

---
+++ number-of-comments-per-post/number-of-comments-per-post.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+select s1.sub_id as post_id, count(distinct s2.sub_id) as number_of_comments
+from submissions s1
+left join submissions s2
+on s1.sub_id = s2.parent_id
+where s1.parent_id is null
+group by s1.sub_id
+order by s1.sub_id
+
+
+
