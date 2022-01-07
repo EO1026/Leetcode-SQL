e7770f0400a02ce73be785c21a2ab6586dd58312
Erica Ouyang <93496446+EO1026@users.noreply.github.com>
2022-01-07 10:29:02 -0500

    Time: 620 ms (74.67%), Space: 0B (100.00%) - LeetHub

--- 1677-products-worth-over-invoices/1677-products-worth-over-invoices.sql
+++ 1677-products-worth-over-invoices/1677-products-worth-over-invoices.sql
@@ -1,5 +1,12 @@
 # Write your MySQL query statement below
-select name,coalesce(sum(rest),0) rest,coalesce(sum(paid),0) paid, coalesce(sum(canceled),0) canceled, coalesce(sum(refunded),0) refunded
-from product p left join invoice i on i.product_id = p.product_id
-group by name order by 1
+#select name,coalesce(sum(rest),0) rest,coalesce(sum(paid),0) paid, coalesce(sum(canceled),0) canceled, coalesce(sum(refunded),0) refunded
+#from product p left join invoice i on i.product_id = p.product_id
+#group by name order by 1
+
+select name, coalesce(sum(rest),0) as rest, coalesce(sum(paid),0) as paid, coalesce(sum(canceled),0) as canceled, coalesce(sum(refunded),0) as refunded
+from product p
+left join invoice i
+on p.product_id = i.product_id
+group by name
+order by 1
 
