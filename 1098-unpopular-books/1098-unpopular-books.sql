e06e6cc30824deec13ebedf7b914b6a26079d066
Erica Ouyang <93496446+EO1026@users.noreply.github.com>
2022-01-26 16:20:15 -0500

    Time: 928 ms (26.11%), Space: 0B (100.00%) - LeetHub

--- 1098-unpopular-books/1098-unpopular-books.sql
+++ 1098-unpopular-books/1098-unpopular-books.sql
@@ -1,10 +1,7 @@
 # Write your MySQL query statement below
-SELECT book_id, name
-FROM Books
-WHERE available_from < '2019-05-23'
-AND book_id NOT IN
-            (SELECT book_id
-             FROM Orders
-             WHERE dispatch_date BETWEEN '2018-06-23' AND '2019-06-23'
-             GROUP BY book_id
-             Having sum(quantity) >= 10) 
+select b.book_id, name
+from books b left join (select * from orders
+where timestampdiff(year, dispatch_date, '2019-06-23') < 1) o using(book_id)
+where timestampdiff(month, available_from, '2019-06-23') > 1
+group by 1
+having ifnull(sum(quantity),0) < 10
