62bf3e54ab218774e81fd993fdb1617513ba9898
Erica Ouyang <93496446+EO1026@users.noreply.github.com>
2022-05-24 19:01:20 -0400

    Time: 690 ms (34.83%), Space: 0B (100.00%) - LeetHub

---
+++ 1398-customers-who-bought-products-a-and-b-but-not-c/1398-customers-who-bought-products-a-and-b-but-not-c.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+select customer_id, customer_name 
+from Customers where customer_id in (select customer_id from Orders where product_name = "A")
+              and customer_id in (select customer_id from Orders where product_name = "B")
+              and customer_id not in (select customer_id from Orders where product_name = "C")
