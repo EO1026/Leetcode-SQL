7724a1a6a3c3704a82d3274e2dfab796136c21b4
Erica Ouyang <93496446+EO1026@users.noreply.github.com>
2022-06-15 19:13:37 -0400

    Time: 1500 ms (5.07%), Space: 0B (100.00%) - LeetHub

---
+++ 2238-number-of-times-a-driver-was-a-passenger/2238-number-of-times-a-driver-was-a-passenger.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+select r1.driver_id, count(distinct r2.ride_id) as cnt
+from rides r1 left join rides r2
+on r1.driver_id=r2.passenger_id
+group by r1.driver_id
