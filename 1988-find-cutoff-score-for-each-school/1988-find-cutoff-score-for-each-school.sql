384ad92d066da9d658c715f93476e874505caa2e
Erica Ouyang <93496446+EO1026@users.noreply.github.com>
2022-06-21 19:01:51 -0400

    Time: 581 ms (73.10%), Space: 0B (100.00%) - LeetHub

--- 1988-find-cutoff-score-for-each-school/1988-find-cutoff-score-for-each-school.sql
+++ 1988-find-cutoff-score-for-each-school/1988-find-cutoff-score-for-each-school.sql
@@ -1,12 +1,7 @@
 # Write your MySQL query statement below
-with a as (select capacity, max(student_count)as student_count
-from schools 
-cross join exam 
-where capacity-student_count>=0
-group by capacity) # list the max student_count matching each capacity
-
-select school_id, ifnull(min(score),-1) as score 
-from schools 
-left join a on schools.capacity=a.capacity  #left join is necessary here because there might schools too small for each student_count
-left join exam on a.student_count=exam.student_count
-group by school_id #group by and min() will resolve the potential situations when two different scores have the same student_count
+select a.school_id, ifnull(a.score, -1) as 'score'
+from
+(select*, rank() over(partition by s.school_id order by (s.capacity-e.student_count) asc, e.score asc) as 'rank'
+from Schools s left join Exam e on s.capacity >=e.student_count
+order by s.school_id) a
+where a.rank = 1
