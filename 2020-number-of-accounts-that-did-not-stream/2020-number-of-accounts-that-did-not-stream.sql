5f3a1b5014340ce6e22b459f7cb845bdffb3b24a
Erica Ouyang <93496446+EO1026@users.noreply.github.com>
2022-06-24 22:23:15 -0400

    Time: 1397 ms (15.95%), Space: 0B (100.00%) - LeetHub

--- 2020-number-of-accounts-that-did-not-stream/2020-number-of-accounts-that-did-not-stream.sql
+++ 2020-number-of-accounts-that-did-not-stream/2020-number-of-accounts-that-did-not-stream.sql
@@ -1,8 +1,6 @@
 # Write your MySQL query statement below
-select count(account_id) as accounts_count
-from(SELECT account_id,session_id
-     FROM Subscriptions left join (select* from Streams 
-                                   where year(stream_date)=2021)a 
-     using (account_id)
-     where year(start_date)=2021 or year(end_date)=2021 )b
-where session_id is null
+select count(distinct sub.account_id) as accounts_count
+from Subscriptions sub 
+left join Streams stream 
+on sub.account_id =  stream.account_id
+where (year(sub.end_date) = 2021 or year(sub.start_date) = 2021) and year(stream.stream_date)!= 2021
